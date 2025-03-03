e7ed629d581a76d215ebda3c2f5a80669b3905a9
edbert <edbert.muis@gmail.com>
2025-03-04 01:02:15 +0700

    update routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const authController = require("../controllers/authController");
+const express = require("express"); //Mengimpor modul Express.js untuk digunakan dalam pembuatan router.
+const authController = require("../controllers/authController"); //Mengimpor authController dari file authController.js yang terletak dalam folder controllers yang dimana memiliki fungsi-fungsi yang menangani autentikasi, seperti registrasi dan login.
 
-const router = express.Router();
+const router = express.Router(); //Membuat instance express.Router(), yang digunakan untuk membuat rute atau endpoint HTTP.
 
-router.post("/register", authController.register);
+router.post("/register", authController.register); //Mengatur proses login dan registrasi.
 router.post("/login", authController.login);
 
-module.exports = router;
+module.exports = router; //Membuat router tersedia untuk digunakan di file lain, biasanya app.js atau server.js.
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require("express"); //Import modul yang dibutuhkan.
 const taskController = require("../controllers/taskController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-const router = express.Router();
+const router = express.Router(); //Untuk membuat router yang berbeda yang bertanggung jawab atas endpoint tugas yang terkait.
 
-router.use(authMiddleware);
+router.use(authMiddleware); //Sebelum diproses lebih lanjut, setiap rute di router ini akan melalui middleware autentikasi.
 
-router.post("/tasks", taskController.createTask);
-router.get("/tasks", taskController.getTasks);
-router.put("/tasks/:id", taskController.updateTask);
-router.delete("/tasks/:id", taskController.deleteTask);
+router.post("/tasks", taskController.createTask); //Untuk membuat tugas baru, gunakan fungsi createTask.
+router.get("/tasks", taskController.getTasks); //Menjalankan fungsi getTasks untuk mendapatkan daftar pekerjaan.
+router.put("/tasks/:id", taskController.updateTask); //Menjalankan fungsi updateTask untuk memperbarui tugas sesuai dengan identitas.
+router.delete("/tasks/:id", taskController.deleteTask); //Menghapus tugas berdasarkan identitas dengan menjalankan fungsi deleteTask.
 
-module.exports = router;
+module.exports = router; //Mengekspor router sehingga dapat digunakan di file tambahan, biasanya app.js atau server.js.
