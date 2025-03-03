2bd4faf5cf56d55ecf18926b94a407bba31b2a98
edbert <edbert.muis@gmail.com>
2025-03-04 00:48:08 +0700

    update models

--- models/task.js
+++ models/task.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //Menghubungkan dan mengelola data MongoDB dengan mengimpor library Mongoose.
 
-const taskSchema = new mongoose.Schema({
+const taskSchema = new mongoose.Schema({ //Membuat skema tugas, yang menunjukkan bagaimana koleksi tugas dilakukan dalam database MongoDB.
     title: String,
     category: String,
     deadline: Date,
@@ -8,22 +8,22 @@ const taskSchema = new mongoose.Schema({
     userId: mongoose.Schema.Types.ObjectId
 });
 
-const taskModel = mongoose.model('tasks', taskSchema);
+const taskModel = mongoose.model('tasks', taskSchema); //Membuat model tugas yanng terhubung ke koleksi tugas dalam database MongoDB dan memungkinkan berinteraksi dengan data tugas.
 
 const Task = {
-    create: async (title, category, deadline, status, userId) => {
+    create: async (title, category, deadline, status, userId) => { //Memperkenalkan tugas baru.
         let task = new taskModel({
             title, category, deadline, status, userId
         });
         task = await task.save();
         return task._id;
     },
-    findAllByUserId: async (userId) => {
+    findAllByUserId: async (userId) => { //Menampilkan semua tugas sesuai dengan ID Pengguna.
         return taskModel.find({
             userId: userId
         }).exec();
     },
-    update: async (taskId, title, category, deadline, status) => {
+    update: async (taskId, title, category, deadline, status) => { //Perbarui tugas berdasarkan identitas.
         return taskModel.findByIdAndUpdate(taskId, {
             $set: {
                 title, category, deadline, status
@@ -32,9 +32,9 @@ const Task = {
             returnDocument: "after"
         }).exec()
     },
-    delete: async (taskId) => {
+    delete: async (taskId) => { //Menghapus tugas yang berdasarkan identitas.
         return taskModel.findByIdAndDelete(taskId).exec();
     },
 };
   
-module.exports = Task;
+module.exports = Task; //Mengekspor objek Tugas untuk digunakan di file aplikasi lainnya.
--- models/user.js
+++ models/user.js
@@ -1,27 +1,27 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose"); //Untuk berinteraksi dengan MongoDB database.
+const bcrypt = require("bcryptjs"); //Untuk mengamankan password dengan meng-hashnya.
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({ //Membuat schema pengguna.
     username: String,
     password: String
 });
 
-userSchema.index({ username:1 }, { unique: true }) // supaya username unique
+userSchema.index({ username:1 }, { unique: true }) //Membuat index unik untuk nama pengguna.
 
-const userModel = mongoose.model('users', userSchema);
+const userModel = mongoose.model('users', userSchema); //Konstruksi Model Mongoose.
 
-const User = {
-    create: async (username, password) => {
-        const hashedPassword = await bcrypt.hash(password, 10); // hash password agar tidak nyimpan plain text dalam database
+const User = { //Untuk mengelola aktivitas database.
+    create: async (username, password) => { //Membuat pengguna baru.
+        const hashedPassword = await bcrypt.hash(password, 10); //Hash password agar tidak nyimpan plain text dalam database.
         let user = new userModel({
             username, password: hashedPassword
         });
         user = await user.save();
         return user._id;
     },
-    findByUsername: async (username) => {
+    findByUsername: async (username) => { //Mencari orang berdasarkan nama pengguna.
         return userModel.findOne({ username }).exec();
     }
 };
 
-module.exports = User;
+module.exports = User; //Mengekspor objek user, yang memungkinkan penggunaan di file lain yang membutuhkan fungsi create dan findByUsername.
